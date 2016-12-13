4aaef88e4cb0a48a351c4abfcb6e40838e55d24e
Myroslava Zubach <Myroslava_Zubach@epam.com>
2016-12-13 15:28:12 +0100

    changes

---
+++ src/main/java/Math.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Myroslava_Zubach on 13.12.2016.
+ */
+public class Math {
+
+    public int summaryOfTwoNumbers(int firstOperand, int secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    public void println(String testVariable) {
+        System.out.println(testVariable);
+    }
+
+}
---
+++ src/main/java/TestApplication.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Myroslava_Zubach on 13.12.2016.
+ */
+public class TestApplication {
+    public static void main(String []args) {
+        System.out.println("Hello World");
+
+        Math testSummary = new Math();
+
+        int result = testSummary.summaryOfTwoNumbers(2,2);
+
+        System.out.println(result);
+
+        System.out.println(Integer.toString(result));
+
+
+        testSummary.println(Integer.toString(result));
+    }
+}
