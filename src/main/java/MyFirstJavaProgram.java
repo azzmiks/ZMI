fb6c17a7cee095642d73eff6321f69dce7714ded
Myroslava_Zubach <myroslava_zubach@epam.com>
2017-01-27 09:27:28 +0100

    MyFirstJavaProgram

---
+++ src/main/java/MyFirstJavaProgram.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Myroslava_Zubach on 27.01.2017.
+ */
+public class MyFirstJavaProgram {
+    public static void main(String []args) {
+        System.out.println("Hello World");
+    }
+}
