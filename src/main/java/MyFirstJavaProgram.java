4450fb0cebd30d380902217cacf7fd49afdbce8c
azzmiks <myroslava_zubach@epam.com>
2017-01-31 23:40:26 +0100

    MyFirstJavaProgram

--- src/main/java/MyFirstJavaProgram.java
+++ src/main/java/MyFirstJavaProgram.java
@@ -1,5 +1,9 @@
 /**
+<<<<<<< Updated upstream
  * Created by Myroslava_Zubach on 27.01.2017.
+=======
+ * Created by azmiks on 26/01/2017.
+>>>>>>> Stashed changes
  */
 public class MyFirstJavaProgram {
     public static void main(String []args) {
