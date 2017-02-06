d5ed73f6b3146eff7270583a1ba9f92b769cff25
azzmiks <myroslava_zubach@epam.com>
2017-02-06 23:46:08 +0100

    renamed MyFirstJavaProgram, DataTypes and Constants

--- src/main/java/L1_FirstVariables.java
+++ src/main/java/L1_FirstVariables.java
@@ -5,7 +5,7 @@
  * Created by azmiks on 26/01/2017.
 >>>>>>> Stashed changes
  */
-public class MyFirstJavaProgram {
+public class L1_MyFirstJavaProgram {
     public static void main (String[] args)
     {
         System.out.println("Hello World");
