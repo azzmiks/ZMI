a57076b9410adfed98e76a63f81254eaed56c076
azzmiks <myroslava_zubach@epam.com>
2017-02-07 21:04:56 +0100

    new class L4_Arithmetic

---
+++ src/main/java/L4_Arithmetic.java
@@ -0,0 +1,23 @@
+/**
+ * Created by azmiks on 07/02/2017.
+ */
+public class L4_Arithmetic {
+    public static void main (String[] args)
+    {
+        int num = 100;
+        int factor = 20;
+        int sum = 0;
+
+        sum = num + factor; // 100+20
+        System.out.println("Sum result: " + sum);
+
+        sum = num - factor; // 100-20
+        System.out.println("Subtruction result: " + sum);
+
+        sum = num * factor; // 100*20
+        System.out.println("Multiplication result: " + sum);
+
+        sum = num / factor; // 100/20
+        System.out.print("Division result: " + sum);
+    }
+}
