5f0b6be68ef0dcb7b5cf5ed13319a544fe6e8c0d
azzmiks <myroslava_zubach@epam.com>
2017-02-11 12:54:23 +0100

    New class L17_DoWhile

---
+++ src/main/java/learningJava/L17_DoWhile.java
@@ -0,0 +1,17 @@
+package learningJava;
+
+/**
+ * Created by azmiks on 11/02/2017.
+ */
+public class L17_DoWhile {
+    public static void main(String[] args) {
+
+        int num = 100;
+
+        do {
+            System.out.println("Count using DoWhile: " + num);
+            num += 10;
+        }
+        while (num < 0);
+    }
+}
