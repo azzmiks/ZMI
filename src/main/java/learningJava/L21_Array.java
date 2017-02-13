08b7b1557677023cd32701d0f253eef07528d54a
azzmiks <myroslava_zubach@epam.com>
2017-02-13 21:32:12 +0100

    New class L21_Array

---
+++ src/main/java/learningJava/L21_Array.java
@@ -0,0 +1,23 @@
+package learningJava;
+
+/**
+ * Created by azmiks on 13/02/2017.
+ */
+public class L21_Array {
+    public static void main(String[] args) {
+
+        String[] str = {"Java ", "is ", "good" };
+
+        int[] num = new int[3];
+
+        num[0] = 100;
+        num[1] = 200;
+
+        str[2] = "awesome";
+
+        System.out.println("String array length: " + str.length);
+        System.out.println("Number array length: " + num.length);
+        System.out.println(num[0] + ", " + num[1] + ", " + num[2]);
+        System.out.println(str[0] + str[1] + str[2]);
+    }
+}
