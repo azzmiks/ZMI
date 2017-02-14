4b423278478dacbb1676d2c1f8b981ee011c1624
azzmiks <myroslava_zubach@epam.com>
2017-02-14 22:39:20 +0100

    New class L33_Lottery

---
+++ src/main/java/learningJava/L33_Lottery.java
@@ -0,0 +1,24 @@
+package learningJava;
+
+/**
+ * Created by azmiks on 14/02/2017.
+ */
+public class L33_Lottery {
+    public static void main(String[] args) {
+
+        int[] nums = new int[50];
+        for (int i=1; i < 50; i++) {
+            nums[i] = i;
+        }
+
+        for (int i=1; i < 50; i++) {
+            int r = (int) Math.ceil(Math.random() * 49);
+            int temp = nums[1];
+            nums[i] = nums[r];
+            nums[r] = temp;
+        }
+        for (int i=1; i < 7; i++) {
+            System.out.print(Integer.toString(nums[i])+ " ");
+        }
+    }
+}
