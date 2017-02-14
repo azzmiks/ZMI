708a862eca51bcbc7177c5475f4a278152019450
azzmiks <myroslava_zubach@epam.com>
2017-02-14 23:03:42 +0100

    New class L34_StringLength

---
+++ src/main/java/learningJava/L34_StringLength.java
@@ -0,0 +1,16 @@
+package learningJava;
+
+/**
+ * Created by azmiks on 14/02/2017.
+ */
+public class L34_StringLength {
+    public static void main(String[] args) {
+
+        String lang = "Java";
+        String series = " in easy steps";
+
+        String title = lang.concat(series);
+
+        System.out.println("\"" + title + "\" contains " + title.length() + " symbols");
+    }
+}
