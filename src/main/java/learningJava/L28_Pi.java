61f6e7b4a95d9ee6c495b5edbc9f009129c15282
azzmiks <myroslava_zubach@epam.com>
2017-02-14 20:19:54 +0100

    New class L28_Pi

---
+++ src/main/java/learningJava/L28_Pi.java
@@ -0,0 +1,20 @@
+package learningJava;
+
+/**
+ * Created by azmiks on 14/02/2017.
+ */
+public class L28_Pi {
+    public static void main(String[] args) {
+
+        float radius = Float.parseFloat(args[0]);
+        float shortPi = (float) Math.PI;
+
+        float circ = shortPi * (radius + radius);
+        float area = shortPi * (radius * radius);
+
+        System.out.println("Range from " + Math.PI + " to " + shortPi);
+        System.out.println("Radius: " + radius + "sm");
+        System.out.println("Circle length: " + circ);
+        System.out.println("Circle square: " + area);
+    }
+}
