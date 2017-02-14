d430be924f247fe4a0cb291d85fd6929c56200c2
azzmiks <myroslava_zubach@epam.com>
2017-02-14 22:04:11 +0100

    New class L31_Compare

---
+++ src/main/java/learningJava/L31_Compare.java
@@ -0,0 +1,15 @@
+package learningJava;
+
+/**
+ * Created by azmiks on 14/02/2017.
+ */
+public class L31_Compare {
+    public static void main(String[] args) {
+
+        float num1 = 24.75f;
+        int num2 = 25;
+
+        System.out.println("Min number: " + Math.min(num1, num2));
+        System.out.println("Max number: " + Math.max(num1, num2));
+    }
+}
