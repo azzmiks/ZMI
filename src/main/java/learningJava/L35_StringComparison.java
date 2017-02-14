91024e8023fce72fa51a004630f83afa0e3de1d4
azzmiks <myroslava_zubach@epam.com>
2017-02-14 23:12:44 +0100

    New class L35_StringComparison

---
+++ src/main/java/learningJava/L35_StringComparison.java
@@ -0,0 +1,25 @@
+package learningJava;
+
+/**
+ * Created by azmiks on 14/02/2017.
+ */
+public class L35_StringComparison {
+    public static void main(String[] args) {
+
+        String password = "bingo";
+        String pass = "BINGO";
+
+        try {
+            if (pass.toLowerCase().equals(password)) {
+                System.out.println("Password is correct");
+            }
+            else {
+                System.out.println("Password is invalid");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Please enter password");
+        }
+
+    }
+}
