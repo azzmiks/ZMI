a980d42c43208775d663069065274005dd66133e
azzmiks <myroslava_zubach@epam.com>
2017-02-15 23:53:25 +0100

    Creating classes in creatingClasses package

---
+++ src/main/java/learningJava/creatingClasses/L42_Car.java
@@ -0,0 +1,15 @@
+package learningJava.creatingClasses;
+
+/**
+ * Created by azmiks on 15/02/2017.
+ */
+public class L42_Car {
+
+    public final static String color = "Red";
+    public final static String bodyType = "Coupe";
+
+    public static String accelerate() {
+        String motion = "Accelerating";
+        return motion;
+    }
+}
