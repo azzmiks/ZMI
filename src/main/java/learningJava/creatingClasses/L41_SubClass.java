c9db0b57db5d6469544be7994d4d6d7e1b32998a
azzmiks <myroslava_zubach@epam.com>
2017-02-15 23:42:06 +0100

    Creating classes in creatingClasses package

---
+++ src/main/java/learningJava/creatingClasses/L41_SubClass.java
@@ -0,0 +1,30 @@
+package learningJava.creatingClasses;
+
+import sun.tools.tree.SuperExpression;
+
+/**
+ * Created by azmiks on 15/02/2017.
+ */
+public class L41_SubClass extends L41_SuperClass {
+
+    public static void main (String[] args) {
+
+        hello();
+        L41_SuperClass.hello();
+        echo(args[0]);
+
+    }
+
+    public static void hello() {
+        System.out.println("Hello from SubClass");
+    }
+
+//    public static void echo(String arg) {
+//        try {
+//            System.out.println("You entered: " + arg);
+//        }
+//        catch (Exception e) {
+//            System.out.println("Argument is needed");
+//        }
+//    }
+}
