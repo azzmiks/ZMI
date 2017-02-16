24423709df11f8e8fee12b38333f643d65fe0a31
azzmiks <myroslava_zubach@epam.com>
2017-02-16 20:22:55 +0100

    Creating classes in importingFunctions package

---
+++ src/main/java/learningJava/importingFunctions/L53_Formats.java
@@ -0,0 +1,32 @@
+package learningJava.importingFunctions;
+
+import javax.swing.text.DateFormatter;
+import java.text.NumberFormat;
+import java.time.DateTimeException;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by azmiks on 16/02/2017.
+ */
+class L53_Formats {
+
+    public static void main(String[] args) {
+
+        NumberFormat nf = NumberFormat.getNumberInstance();
+        System.out.println("\nNumber: " + nf.format(123456789));
+
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+        System.out.println("\nCurrency: " + cf.format(1234.50f));
+
+        NumberFormat pf = NumberFormat.getPercentInstance();
+        System.out.println("\nPercents: " + pf.format(0.75f));
+
+        java.time.LocalDateTime now = java.time.LocalDateTime.now();
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("MMM d, yyy");
+        System.out.println("\nDate: " + now.format(df));
+
+        DateTimeFormatter tf = DateTimeFormatter.ofPattern("h:m a");
+        System.out.println("\nTime: " + now.format(tf));
+    }
+}
