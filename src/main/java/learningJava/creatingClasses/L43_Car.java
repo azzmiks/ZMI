5b37291fdb0fc65c2c21a1ea7194e57d51352bc5
Myroslava_Zubach <myroslava_zubach@epam.com>
2017-02-16 12:27:06 +0100

    Creating classes in creatingClasses package

---
+++ src/main/java/learningJava/creatingClasses/L43_Car.java
@@ -0,0 +1,15 @@
+package learningJava.creatingClasses;
+
+/**
+ * Created by Myroslava_Zubach on 16.02.2017.
+ */
+class L43_Car {
+
+    public final static String color = "Red";
+    public final static String bodyType = "Coupe";
+
+    public static String accelerate() {
+        String motion = "Accelerating";
+        return motion;
+    }
+}
---
+++ src/main/java/learningJava/creatingClasses/L43_FirstInstance.java
@@ -0,0 +1,20 @@
+package learningJava.creatingClasses;
+
+/**
+ * Created by Myroslava_Zubach on 16.02.2017.
+ */
+class L43_FirstInstance {
+    public static void main(String[] args) {
+
+        System.out.println("Color: " + L43_Car.color);
+        System.out.println("Bodytype: " + L43_Car.bodyType);
+        System.out.println(L42_Car.accelerate());
+
+        L43_Car class
+        L43_Car Porsche = new L42_Car();
+
+        System.out.println("Porsche color: " + Porsche.color);
+        System.out.println("Porsche bodytype: " + Porsche.bodyType);
+        System.out.println(Porsche.accelerate());
+    }
+}
