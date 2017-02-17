89606fdaa618fa08bcf4f372d8df07b7a449b52a
Myroslava_Zubach <myroslava_zubach@epam.com>
2017-02-17 15:37:59 +0100

    New class Labels

---
+++ src/main/java/learningJava/buildingInterfaces/Labels.java
@@ -0,0 +1,38 @@
+package learningJava.buildingInterfaces;
+
+import javax.swing.*;
+
+/**
+ * Created by Myroslava_Zubach on 17.02.2017.
+ */
+class Labels extends JFrame {
+    JPanel pnl = new JPanel();
+
+    ImageIcon duke = new ImageIcon("C:\\Users\\Myroslava_Zubach\\IdeaProjects\\ZMI\\src\\main\\resources\\duke.png");
+
+    JLabel lbl1 = new JLabel(duke);
+    JLabel lbl2 = new JLabel("Duke, the Java Mascot");
+    JLabel lbl3 = new JLabel("Duke", duke, JLabel.CENTER);
+
+    public Labels() {
+
+        super("Window Swing");
+        setSize(500, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(pnl);
+        setVisible(true);
+
+        lbl1.setToolTipText("Duke, the Java Mascot");
+        lbl3.setHorizontalTextPosition(JLabel.CENTER);
+        lbl3.setVerticalTextPosition(JLabel.BOTTOM);
+
+        pnl.add(lbl1);
+        pnl.add(lbl2);
+        pnl.add(lbl3);
+    }
+
+    public static void main(String[] args) {
+
+        Labels gui = new Labels();
+    }
+}
